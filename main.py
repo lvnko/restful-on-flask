565e1c5f5140aaafa566f01a9ff288c38a37d4b5
lvnko <kevinlao.lo@gmail.com>
2025-04-30 17:33:57 -0300

    Jinja Template 練習。

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask("myapp", template_folder="./templates")
 app.config.from_object('config.DevelopmentConfig')
 
 @app.route('/')
+def login():
+    return render_template("form.html")
+
+@app.route('/index_v1', methods=["POST"])
 def index():
-    return render_template("template.html", username="lvnko")
+    return render_template("index.html", username=request.form["username"])
 
 @app.route('/users')
 def users():
