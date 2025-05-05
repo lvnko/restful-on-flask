df61321b887f0309c88b3714f7ac8776ab8698d2
lvnko <kevinlao.lo@gmail.com>
2025-05-05 12:38:16 -0300

    加入 Message queue 的模擬代碼。

--- refactor.py
+++ refactor.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from models.Users import UserModel
 from resources.UserResources import UserResources
 from resources.ClassResources import ClassResources
+from resources.MessageResources import MessageResources
 
 app = Flask("myapp", template_folder="./templates")
 app.config.from_object('config.DevelopmentConfig')
@@ -10,6 +11,7 @@ api = Api(app)
 
 api.add_resource(UserResources, "/users", '/users/<int:user_id>')
 api.add_resource(ClassResources, "/classes", '/classes/<int:class_id>')
+api.add_resource(MessageResources, "/messages/<int:user_id>")
 
 
 if __name__ == '__main__':
---
+++ resources/MessageResources.py
@@ -0,0 +1,32 @@
+from flask_restful import Resource, reqparse, request
+
+class MessageResources(Resource):
+    # Define queue as a class variable
+    queue = [] 
+    
+    def __init__(self):
+        # Remove queue initialization from __init__
+        self.parser = reqparse.RequestParser(bundle_errors=True)
+        self.parser.add_argument("data_date", type=str, help="Data date is required and must be a string.", required=True)
+        self.parser.add_argument("location", type=str, help="Location is required and must be a string.", required=True)
+
+    def post(self, user_id):
+        token = request.headers.get("token")
+        if token is None:
+            return None, 401, {"WWW-Authenticate": "Token required"}
+        elif not self.is_valid_token(token):
+            return None, 403, {"WWW-Authenticate": "Invalid token"}
+        else:
+            args = self.parser.parse_args()
+            # Append to the class variable queue
+            MessageResources.queue.append({ 
+                "user_id": user_id,
+                "data_date": args.get("data_date"),
+                "location": args.get("location")
+            })
+            # Print the class variable queue
+            print(MessageResources.queue) 
+            return "Acknowledged", 202
+    
+    def is_valid_token(self, token):
+        return token == "xuemi-token"
